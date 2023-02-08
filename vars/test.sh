63e719d6fb1daef3b1b651216abbb11b787868e7
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-02-08 18:21:08 -0500

    update

---
+++ vars/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash  
+    echo "params1=$params"
+    echo "Inventory2=${env.Inventory}"
+    echo "myFilesfull3=${env.myFilesfull}"
+    echo "myFilenames4=${env.myFilenames}"
+    echo "myFilenames5=$myFilenames"
+    echo "Tool_install6=$Tool_install"
