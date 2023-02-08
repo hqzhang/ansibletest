66a5b9824a257c4a8a47fdf1e069423738343184
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-02-08 17:36:50 -0500

    update

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -1,11 +1,16 @@
-
-
-def getvar(String myfiles){
-  return myfiles.split(',').collect{ it.trim()}.collect{ it.split('/').last() }
-
-  }
-
-def getenv(var){
-
-   return var;
+// vars/sayHello.groovy
+def call(String name = 'human') {
+    // Any valid steps can be called from this code, just like in other
+    // Scripted Pipeline
+    echo "Hello, ${name}."
 }
+
+def getFilename(String myfiles){
+   return myfiles.split(',').collect{ it.trim()}.collect{ it.split('/').last() }
+}
+
+def getFiles(String myfiles, String mypath){
+    return myfiles.split(',').collect{  it.trim()}.collect{ mypath+it.split('/').last() }
+}
+
+return this
