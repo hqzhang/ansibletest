1b6a5d761e1b9f38d2c5699a96466ed6c6800eaf
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-02-08 18:38:19 -0500

    update

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 
-print("params1=",params)
-print( "Inventory2=",Inventory) 
+#print("params1=",params)
+#print( "Inventory2=",Inventory) 
 print("myFilesfull3=",myFilesfull)
 print("myFilenames4=",myFilenames)
 print("myFilenames5=",myFilenames)
