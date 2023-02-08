342cf058a012c02b1bdd9b21c39bb5ed364fe6b4
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-02-08 18:29:12 -0500

    update

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash  
     echo "params1=$params"
-    echo "Inventory2=${env.Inventory}"
-    echo "myFilesfull3=${env.myFilesfull}"
-    echo "myFilenames4=${env.myFilenames}"
+    echo "Inventory2=${Inventory}"
+    echo "myFilesfull3=${myFilesfull}"
+    echo "myFilenames4=${myFilenames}"
     echo "myFilenames5=$myFilenames"
     echo "Tool_install6=$Tool_install"
