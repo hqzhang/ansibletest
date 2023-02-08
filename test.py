7aa59712a3e7a2143ce6bf60a1e99e3146c9cb08
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-02-08 18:35:18 -0500

    update

---
+++ test.py
@@ -0,0 +1,7 @@
+
+print("params1=",params)
+print( "Inventory2=",Inventory) 
+print("myFilesfull3=",myFilesfull)
+print("myFilenames4=",myFilenames)
+print("myFilenames5=",myFilenames)
+print("Tool_install6=",Tool_install)
