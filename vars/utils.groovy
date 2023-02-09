e93cc8e7fe967229a0868f1373ed5f93c4343c59
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-02-09 12:38:36 -0500

    update

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -8,19 +8,24 @@ def call(String name = 'human') {
 def getMyFiles(String myfiles){
     echo  "enter getMyFiles with111: $myfiles"
    
-    return myfiles.split(',').collect{ it.trim() }
+    //return myfiles.split(',').collect{ it.trim() }
+    return "getMyFiles"
 }
 
 def getFileNames(String myfiles){
     echo  "enter getFileNames with222: $myfiles"
    
-    return myfiles.split(',').collect{ it.trim() }.collect{ it.split('/').last() }
+    //return myfiles.split(',').collect{ it.trim() }.collect{ it.split('/').last() }
+    return "getFileNames"
+}
 }
 
 def getNewFiles(String myfiles, String mypath){
     echo "enter getNewFiles with333: $mypath"
     def ret=myfiles.split(',').collect{ it.trim()}.collect{ mypath+it.split('/').last() }.join(' ')
-    return "\"$ret\""
+    //return "\"$ret\""
+    return "getNewFiles"
+
 }
 def getGlobals(){
     echo "Enter getGlobal variables:"
