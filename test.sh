8785d09e7eda444ecff8d1617774c8d6bc18a14f
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-02-09 10:44:33 -0500

    update

--- test.sh
+++ test.sh
@@ -1,11 +1,10 @@
 #!/bin/bash  
     echo "params1=$params"
     echo "Inventory2=${Tool_install}"
-    echo ""
     echo "myFilesfull3=${myFilesfull}"
     echo "myFilenames4=${myFilenames}"
     echo "myFilenames5=$myFilenames"
     echo "Tool_install6=$Tool_install"
-   echo "MY_VAR_PAR=${params.MY_VAR}"
-    echo "MY_VAR_ENV=${env.MY_VAR}"
+    echo "MY_VAR_PAR=${MY_VAR}"
+    echo "MY_VAR_ENV=${MY_VAR}"
     echo "MY_VAR=${MY_VAR}"
