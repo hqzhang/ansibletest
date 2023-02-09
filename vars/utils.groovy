d55948927da44167571980c4fbe296cc78d2fe6c
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-02-09 14:51:58 -0500

    update

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -4,8 +4,41 @@ def call(String name = 'human') {
     // Scripted Pipeline
     echo "Hello, ${name}."
 }
-
 def getMyFiles(String myfiles){
+    echo  "enter getMyFiles with111: $myfiles"
+    def str=new String(myfiles)
+    List tmp=[]
+    str.split(',').each {
+      tmp.add(it.trim() )
+    }
+    return str.split(',').collect{ it.trim() }
+    //eturn myfiles
+}
+
+def getFileNames(String myfiles){
+    echo  "enter getFileNames with222: $myfiles"
+   def str=new String(myfiles)
+    List tmp=[]
+    str.split(',').each {
+      tmp.add(it.trim() )
+    }
+    return str.split(',').collect{ it.trim().split('/').last() }
+    //return myfiles
+}
+
+
+def getNewFiles(String myfiles, String mypath){
+    echo "enter getNewFiles with333: $mypath"
+    def str=new String(myfiles)
+    List tmp=[]
+    str.split(',').each {
+      tmp.add(it.trim() )
+    }
+    def var=tmp.collect{ mypath + it.split('/').last() }.join(' ')
+    return "\"${var}\""
+    //return myfiles
+}
+/*def getMyFiles(String myfiles){
     echo  "enter getMyFiles with111: $myfiles"
     def str=new String(myfiles)
     List tmp=[]
@@ -39,7 +72,7 @@ def getNewFiles(String myfiles, String mypath){
     def var=tmp.collect{ mypath + it.split('/').last() }.join(' ')
     return "\"${var}\""
     //return myfiles
-}
+}*/
 def getGlobals(){
     echo "Enter getGlobal variables:"
     echo "params1=$params"
