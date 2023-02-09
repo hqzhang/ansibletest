0fabbef9cf31e351a060a5e8ff98901e3aa9663d
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-02-09 14:27:48 -0500

    update

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -37,7 +37,7 @@ def getNewFiles(String myfiles, String mypath){
       tmp.add(it.trim() )
     }
     tmp.collect{ mypath + it.split('/').last() }
-    return tmp
+    return """+tmp.join(' ')+"""
     //return myfiles
 }
 def getGlobals(){
