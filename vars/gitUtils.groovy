51071bb276114a80abfd0dade9c8222902bc0042
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-02-23 21:32:27 -0500

    update

--- vars/gitUtils.groovy
+++ vars/gitUtils.groovy
@@ -2,8 +2,8 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
 import groovy.json.jsonSlurper
-import groovy.transform.field
-@groovy.transform.field
+import groovy.transform.Field
+@groovy.transform.Field
 def repoPR="https://bitbucket.org/rest/api/1.0/project/myproject/repos/myrepo/pull-requests"
 @NonCPS
 def call(String src,  String workbr, String mergebr, String dir) {
