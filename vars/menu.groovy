dc2e57cda34721e5ce3689830c6187c8e59d6eb1
Hongqi Zhang <zhanghongqi@hotmail.com>
2023-04-15 13:20:47 -0400

    update

--- vars/menu.groovy
+++ vars/menu.groovy
@@ -10,17 +10,20 @@ String convertScript( String str){
 }
 
 def getFileContent(String SolutionDetail){
-    def mf ="ls /Users/hongqizhang/.jenkins/workspace/agroovytest/releases  ".execute().text
+    def mf ="ls /var/root/.jenkins/workspace/workspace/ansibletest/releases  ".execute().text
     def myls = mf.readLines().collect{ it.split()[0].minus('.xml')}
     def map=[:]
-    myls.each { map[it]="curl -k https://raw.githubusercontent.com/hqzhang/ansibletest/main/releases/${it}.xml".execute().text }
+    myls.each { 
+        map[it]="curl -k https://raw.githubusercontent.com/hqzhang/ansibletest/main/releases/${it}.xml".execute().text 
+        map[it]="cat /var/root/.jenkins/workspace/workspace/ansibletest/releases/${it}.xml"
+    }
     def ret= "<textarea name=\"value\"  value  class=\"setting-input  \" type=\"text\">${map[SolutionDetail]}</textarea>"
     convertScript(ret)
 }
 
 def getFileList(){
     def test=''
-    def mf ="ls /Users/hongqizhang/.jenkins/workspace/agroovytest/releases".execute().text
+    def mf ="ls /var/root/.jenkins/workspace/workspace/ansibletest".execute().text
     def out=mf.readLines().collect{ it.split("\\.")[0]}
     return out
 }
